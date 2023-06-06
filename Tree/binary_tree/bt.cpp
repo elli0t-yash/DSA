18b32756a9ae721f8a1eb296a259cc6fb31d48fe
Yash Mishra <yash01012002@gmail.com>
2023-06-06 14:08:22 +0530

    width of the binary tree

--- Tree/binary_tree/bt.cpp
+++ Tree/binary_tree/bt.cpp
@@ -230,6 +230,33 @@ vector<vector<int>> verticalTraversal(Node* root) {
     return ans;
 }
 
+// Maximum width of the tree
+    int widthOfBinaryTree(Node* root) {
+        if(!root)
+            return 0;
+        int ans = 0;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+        while(!q.empty()) {
+            int size = q.size();
+            int mmin = q.front().second;
+            int first,last;
+            for(int i = 0; i < size; i++) {
+                int cur_id = q.front().second - mmin;
+                Node* node = q.front().first;
+                q.pop();
+                if(i == 0) first = cur_id;
+                if(i == size - 1) last = cur_id;
+                if(node -> left) 
+                    q.push({node -> left, cur_id*2+1});
+                if(node -> right)
+                    q.push({node -> right, cur_id*2+2});
+            }
+            ans = max(ans, last - first + 1);
+        }
+        return ans;
+    }
+
 // Path from root to any node X
 bool findPath(Node* root, vector<int> &arr, int x) {
     if(!root)
