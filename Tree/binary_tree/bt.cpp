4464b4a5a7c0b12fd30a0086971665affea2f4de
Yash Mishra <yash01012002@gmail.com>
2023-06-07 10:58:54 +0530

    level order traversal

--- Tree/binary_tree/bt.cpp
+++ Tree/binary_tree/bt.cpp
@@ -282,6 +282,29 @@ vector<int> pathInATree(Node* root, int x) {
     }
 }
 
+// Level order traversal
+vector<vector<int>> levelOrder(Node* root) {
+    vector<vector<int>> ans;
+    if(root) {
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+            for(int i = 0; i < size; i++) {
+                Node* node = q.front();
+                q.pop();
+                if(node -> left) q.push(node -> left);
+                if(node -> right) q.push(node -> right);
+                level.push_back(node -> data);
+            }
+            ans.push_back(level);
+        }
+    }
+        
+    return ans;
+}
+
 int main() {
     Node* root = new Node(2);
     root->left = new Node(4);
