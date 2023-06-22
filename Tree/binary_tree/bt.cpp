ffb20a2f68977b2195aac50953b48ad88f6d749e
Yash Mishra <yash01012002@gmail.com>
2023-06-22 18:49:50 +0530

    Zig-Zag

--- Tree/binary_tree/bt.cpp
+++ Tree/binary_tree/bt.cpp
@@ -371,9 +371,43 @@ bool isSameTree(Node* p, Node* q) {
     if(p == NULL || q == NULL) {
         return(p==q);
     }
-    return(p -> data == q -> data) 
+    return(p -> data == q -> data)
         && isSameTree(p -> left, q -> left) 
         && isSameTree(p -> right, q-> right);
+}
+
+// Zig-Zag traversal 
+vector<vector<int>> zigzagLevelOrder(Node* root) {
+    vector<vector<int>> result;
+    if(root == NULL) {
+        return result;
+    }
+
+    queue<Node*> nodesQueue;
+    nodesQueue.push(root);
+    bool leftToRight = true;
+
+    while(!nodesQueue.empty()) {
+        int size = nodesQueue.size();
+        vector<int> row(size);
+        for (int i = 0; i < size; i++) {
+            Node* node = nodesQueue.front();
+            nodesQueue.pop();
+
+            int index = (leftToRight) ? i : (size - 1- i);
+
+            row[index] = node -> data;
+            if(node -> left) {
+                nodesQueue.push(node -> left);
+            }
+            if(node -> right) {
+                nodesQueue.push(node -> right);
+            }
+        }
+        leftToRight = !leftToRight;
+        result.push_back(row);
+    }
+    return result;
 }
 
 int main() {
