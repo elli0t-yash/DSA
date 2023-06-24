9a9364d33bd68b1889d1b71a0adc00477e1a8fb9
Yash Mishra <yash01012002@gmail.com>
2023-06-25 00:14:43 +0530

    Boundary Traversal

--- Tree/binary_tree/bt.cpp
+++ Tree/binary_tree/bt.cpp
@@ -410,6 +410,50 @@ vector<vector<int>> zigzagLevelOrder(Node* root) {
     return result;
 }
 
+// Boundary Traversal (Anti-clockwise direction)
+void addLeftBoundary(Node* root, vector<int> &res) {
+    Node* cur = root -> left;
+    while (cur) {
+        if(!isLeaf(cur)) res.push_back(cur -> data);
+        if(cur -> left) cur = cur -> left;
+        else cur = cur -> right;
+    }
+}
+
+void addRigthBoundary(Node* root, vector<int> &res) {
+    Node* cur = root -> rigth;
+    vector<int> tmp;
+    while(cur) {
+        if(!isLeaf(cur)) tmp.push_back(cur -> data);
+        if(cur -> right) cur = cur -> right;
+        else cur = cur -> left;
+    }
+    for(int i = tmp.size() - 1; i >= 0; --i) {
+        res.push_back(tmp[i]);
+    }
+}
+
+void addLeaves(Node* root, vector<int> &res) {
+    if(isLeaf(root)) {
+        res.push_back(root -> data);
+        return;
+    }
+
+    if(root -> left) addLeaves(root -> left, res);
+    if(root -> right) addLeaves(root -> right, res);
+}
+
+
+vector<int> traverseBoundary(Node* root){
+    vector<int> res;
+    if(!root) return res;
+    if(!isLeaf(root)) res.push_back(root -> data);
+    addLeftBoundary(root, res);
+    addLeaves(root, res);
+    addRightBoundary(root, res);
+    return res;
+}
+
 int main() {
     Node* root = new Node(2);
     root->left = new Node(4);
